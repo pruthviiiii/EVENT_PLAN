6def587237d0daa7771b0081595cd50ad9340049
pruthviiiii <pruthviraj3234@gmail.com>
2025-01-20 13:03:49 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,13 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.get("/",(req,res)=>{
+    res.json({
+        success:true,
+        message:"hallow sir"
+    });
+});
+
 
 app.use("/api/v1/message", messageRouter);
 
